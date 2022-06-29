ec367f73b7d78334daea662d81ed83c230224d7c
JonieRadiante <105399241+JonieRadiante@users.noreply.github.com>
2022-06-29 04:34:57 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,16 +3,21 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
+var variavel = [1, 'jonie', true, false, function(){}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
+function myFunction(arr) {
+  return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
+myFunction(variavel)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -21,17 +26,31 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
+function myFunction2 (arr, nunber) {
+  return arr[nunber];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
+var myvar = [1.0, 'game', true, {cor: 'azul'}, [2, 3]];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+myFunction2(myvar, 0)
+myFunction2(myvar, 1)
+myFunction2(myvar, 2)
+myFunction2(myvar, 3)
+myFunction2(myvar, 4)
+myFunction2(myvar, 0);
+myFunction2(myvar, 1);
+myFunction2(myvar, 2);
+myFunction2(myvar, 3);
+myFunction2(myvar, 4)
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -48,11 +67,32 @@ propriedades:
 os livros.
 */
 // ?
+function book (bookName) {
+  var threeBooks = {
+    'A volta dos que nao foram': {
+      quantidadeDePaginas: 100,
+      autor: 'Jonie Radiante',
+      editora: 'Radiants Edition'
+    },
+ 'As trancas do rei careca': {
+      quantidadeDePaginas: 50,
+      autor: 'Jonie Radiante',
+      editora: 'Radiants Edition'
+    },
+  'Poeira em alto mar': {
+      quantidadeDePaginas: 10,
+      autor: 'Jonie Radiante',
+      editora: 'Radiants Edition'
+    }
+  };
+  return !bookName ? threeBooks : threeBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
+book()
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
@@ -60,6 +100,7 @@ usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
 // ?
+'O livro ' + bookName +' tem ' + book(bookName).quantidadeDePaginas + ' páginas!'
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
@@ -67,6 +108,7 @@ a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
 // ?
+'O autor do livro  ' + bookName + ' é ' + book(bookName).autor + '.'
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
@@ -74,3 +116,4 @@ a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
 // ?
+'O livro ' + bookName + ' foi publicado pela editora ' + book(bookName).editora + '.'
